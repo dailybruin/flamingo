3b346bebafad37770f3d7dac0bbf035858b510c0
alicelee1227 <56212630+alicelee1227@users.noreply.github.com>
2019-11-05 17:21:28 -0800

    Added InTheNewsCard component

--- pages/playground.jsx
+++ pages/playground.jsx
@@ -3,13 +3,26 @@ import fetch from 'isomorphic-unfetch'
 import Error from 'next/error'
 import Link from 'next/link'
 
-import ArticleCard from '../components/ArticleCard'
+import InTheNews from '../components/InTheNews'
+
+let news = [
+  //<a href="https://dailybruin.com/2019/10/29/investigation-suggests-getty-fire-started-by-tree-branch-falling-on-power-lines/">Getty Fire</a>, 
+  //<a href="https://dailybruin.com/2019/10/31/ucla-mens-soccer-to-face-three-games-it-must-win-to-qualify-for-the-ncaa/">Men's Soccer Championship</a>
+  {
+    name: "Getty Fire",
+    href: "https://dailybruin.com/2019/10/29/investigation-suggests-getty-fire-started-by-tree-branch-falling-on-power-lines/"
+  },
+  {
+    name: "Soccer",
+    href: "https://dailybruin.com/2019/10/31/ucla-mens-soccer-to-face-three-games-it-must-win-to-qualify-for-the-ncaa/"
+  }
+]
 
 export default class Playground extends Component {
   render() {
     return (
       <div style={{margin: "auto", maxWidth: "1236px"}}>
-        {/* Put Component Here */}
+        <InTheNews stories={news}></InTheNews>
       </div>
     )
   }
