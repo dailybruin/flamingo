92576a46476de3ae7ec8b10d4fc6521f45170ca7
tom22ger <thomasadrian@mac.com>
2020-06-02 16:54:43 -0700

    added /records-requests

---
+++ pages/records-requests.jsx
@@ -0,0 +1,19 @@
+import PageWrapper from "../layouts/PageWrapper";
+import React, { Component } from "react";
+import { Config } from "../config.js";
+
+import PageLayout from "../layouts/Page";
+
+class Page extends Component {
+  static async getInitialProps(context) {
+    const { slug } = context.query;
+    const pageRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/pages/171179`);
+    const page = await pageRes.json();
+    return { page };
+  }
+  render() {
+    return <PageLayout page={this.props.page} />;
+  }
+}
+
+export default PageWrapper(Page);
