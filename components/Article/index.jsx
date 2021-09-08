8311b4c2fb382d4800021c7cfe78930fa9f4ac6c
Tommy Vo Tran <tomvotran@ucla.edu>
2021-09-07 19:26:42 -0700

    Removed test msg

--- components/Article/index.jsx
+++ components/Article/index.jsx
@@ -206,7 +206,7 @@ export default class Article extends React.Component {
                   }
                 `}
               >
-                By (test_msg) {renderAuthors(this.props.authors)}
+                By {renderAuthors(this.props.authors)}
               </h3>
               <h4
                 css={css`
