e0ac3167c730324428444856d2868b6d51a9f806
Vandita Sethuraman <vandita.sethuraman@gmail.com>
2022-02-10 18:57:25 -0800

    changed size of sponsored links bar and font sizes of header and links

--- components/SponsoredLinks/index.jsx
+++ components/SponsoredLinks/index.jsx
@@ -15,12 +15,13 @@ export default class SponsoredLinks extends React.Component {
           background: #fff;
           box-shadow: ${globals.cardShadow};
           padding: 10px;
+          width: 202%;
         `}
       >
         <h1
           css={css`
             font-family: ${globals.menuFont};
-            font-size: 14px;
+            font-size: 18px;
             margin: 0 0 4px;
           `}
         >
@@ -29,7 +30,7 @@ export default class SponsoredLinks extends React.Component {
         <div
           css={css`
             font-family: ${globals.menuFont};
-            font-size: 10px;
+            font-size: 13px;
             list-style: none;
             column-count: 3;
             a {
