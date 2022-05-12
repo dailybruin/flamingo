47777034dad61ee5cff90221e952a7a4ebee1e9a
Tommy Vo Tran <tomvotran@ucla.edu>
2022-05-11 23:57:23 -0700

    added illo and interactive

--- layouts/TheStateOfTheUC/index.jsx
+++ layouts/TheStateOfTheUC/index.jsx
@@ -15,6 +15,7 @@ import columnbg from "./columnbg.svg";
 import columnbgmobile from "./columnbgmobile.svg";
 import storiesbg from "../AAPI2022/storiesbg.svg";
 import storiesbgmobile from "../AAPI2022/storiesbgmobile.svg";
+import UCEnrollment from "./Interactives";
 
 export default class TheStateOfTheUCLayout extends React.Component {
   constructor(props) {
@@ -104,76 +105,74 @@ export default class TheStateOfTheUCLayout extends React.Component {
     }
     return (
       <>
-        <div>
-          <a href="/">
-            <img
-              src={DesktopImg}
-              css={css`
-                margin: auto;
-                width: 100%;
-                display: block;
-                ${mediaQueries.mobile} {
-                  display: none;
-                }
-              `}
-            />
-            <img
-              src={MobileImg}
-              css={css`
-                margin: auto;
-                width: 100%;
-                display: block;
-                ${mediaQueries.notMobile} {
-                  display: none;
-                }
-              `}
-            />
-          </a>
+        <a href="/">
+          <img
+            src={DesktopImg}
+            css={css`
+              margin: auto;
+              width: 100%;
+              display: block;
+              ${mediaQueries.mobile} {
+                display: none;
+              }
+            `}
+          />
+          <img
+            src={MobileImg}
+            css={css`
+              margin: auto;
+              width: 100%;
+              display: block;
+              ${mediaQueries.notMobile} {
+                display: none;
+              }
+            `}
+          />
+        </a>
 
+        <div
+          css={css`
+            background-color: #ffe249;
+            color: #000;
+            font-weight: bold;
+            padding: 20px 20px;
+            font-family: "Open Sans", ${globals.bodyFont};
+            overflow: auto;
+            display: block;
+          `}
+        >
           <div
             css={css`
-              background-color: #ffe249;
+              background-color: #fff1a5;
               color: #000;
-              font-weight: bold;
-              padding: 20px 20px;
+              padding: 15px 35px;
               font-family: "Open Sans", ${globals.bodyFont};
-              overflow: auto;
-              display: block;
+              /* background-color: red; */
             `}
           >
-            <div
+            <p
               css={css`
-                background-color: #fff1a5;
-                color: #000;
-                padding: 15px 35px;
-                font-family: "Open Sans", ${globals.bodyFont};
-                /* background-color: red; */
+                width: 100%;
+                font-weight: normal;
+                font-size: 24px;
+                line-height: 1.3;
+                @media all and (max-width: 800px) {
+                  font-size: 12px;
+                }
               `}
             >
-              <p
-                css={css`
-                  width: 100%;
-                  font-weight: normal;
-                  font-size: 24px;
-                  line-height: 1.3;
-                  @media all and (max-width: 800px) {
-                    font-size: 12px;
-                  }
-                `}
-              >
-                The University of California has ambitious goals to reshape the
-                current state of UC campuses. In the coming years, twice the
-                amount of students could be walking on campuses that get most of
-                their energy from electricity rather than fossil fuels. A
-                student at one UC campus could have access to the same resources
-                as a student at another UC campus. However, while the UC has
-                made strides to change over the years, there is still much work
-                to be done according to community members across the system. In
-                “The state of the UC,” UC faculty and students highlight
-                existing issues and explore potential solutions related to
-                funding, sustainability and enrollment.
-              </p>
-            </div>
+              The University of California has ambitious goals to reshape the
+              current state of UC campuses. In the coming years, twice the
+              amount of students could be walking on campuses that get most of
+              their energy from electricity rather than fossil fuels. A student
+              at one UC campus could have access to the same resources as a
+              student at another UC campus. However, while the UC has made
+              strides to change over the years, there is still much work to be
+              done according to community members across the system. In “The
+              state of the UC,” UC faculty and students highlight existing
+              issues and explore potential solutions related to funding,
+              sustainability and enrollment.
+            </p>
           </div>
         </div>
         <div
@@ -200,6 +199,7 @@ export default class TheStateOfTheUCLayout extends React.Component {
             {renderedPosts}
           </div>
         </div>
+        <UCEnrollment />
       </>
     );
   }
