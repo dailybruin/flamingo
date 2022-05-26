6eabaa5de8aae6d4e2584f66eb68494b8b177973
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-05-25 19:59:19 -0700

    move the darkmode switch to the left

--- components/Masthead/Desktop.jsx
+++ components/Masthead/Desktop.jsx
@@ -148,6 +148,67 @@ export default class Desktop extends React.Component {
               @media (max-width: 900px) {
                 display: none;
               }
+              .switch {
+                position: relative;
+                display: inline-block;
+                margin-top: 20px;
+                margin-left: 20px;
+                width: 30px;
+                height: 17px;
+              }
+              .switch input {
+                opacity: 0;
+                width: 0;
+                height: 0;
+              }
+
+              /* The slider */
+              .slider {
+                position: absolute;
+                cursor: pointer;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                background-color: #ccc;
+                -webkit-transition: .4s;
+                transition: .4s;
+              }
+
+              .slider:before {
+                position: absolute;
+                content: "";
+                height: 13px;
+                width: 13px;
+                left: 2px;
+                bottom: 2px;
+                background-color: white;
+                -webkit-transition: .4s;
+                transition: .4s;
+              }
+
+              input:checked + .slider {
+                background-color: #2196F3;
+              }
+
+              input:focus + .slider {
+                box-shadow: 0 0 1px #2196F3;
+              }
+
+              input:checked + .slider:before {
+                -webkit-transform: translateX(13px);
+                -ms-transform: translateX(13px);
+                transform: translateX(13px);
+              }
+
+              /* Rounded sliders */
+              .slider.round {
+                border-radius: 17px;
+              }
+
+              .slider.round:before {
+                border-radius: 50%;
+              }
             `}
           >
             <h2
@@ -163,14 +224,12 @@ export default class Desktop extends React.Component {
             >
               {today}
             </h2>
+            <label class="switch">
+              <input type="checkbox" checked={this.props.darkmode} onChange={(e)=>this.props.onToggle(e)}/>
+              <span class="slider round"></span>
+            </label>
           </div>
-          {/* <div
-            css={css`
-              display: table-cell;
-              text-align: center;
-              white-space: nowrap;
-            `}
-          ></div> TODOOOOO*/}
+          
           <div
             css={css`
               display: table-cell;
@@ -285,73 +344,9 @@ export default class Desktop extends React.Component {
                     & a:hover img {
                       fill: ${globals.DBblue};
                     }
-                    .switch {
-                      position: relative;
-                      display: inline-block;
-                      width: 30px;
-                      height: 17px;
-                    }
-                    .switch input {
-                      opacity: 0;
-                      width: 0;
-                      height: 0;
-                    }
-
-                    /* The slider */
-                    .slider {
-                      position: absolute;
-                      cursor: pointer;
-                      top: 0;
-                      left: 0;
-                      right: 0;
-                      bottom: 0;
-                      background-color: #ccc;
-                      -webkit-transition: .4s;
-                      transition: .4s;
-                    }
-
-                    .slider:before {
-                      position: absolute;
-                      content: "";
-                      height: 13px;
-                      width: 13px;
-                      left: 2px;
-                      bottom: 2px;
-                      background-color: white;
-                      -webkit-transition: .4s;
-                      transition: .4s;
-                    }
-
-                    input:checked + .slider {
-                      background-color: #2196F3;
-                    }
-
-                    input:focus + .slider {
-                      box-shadow: 0 0 1px #2196F3;
-                    }
-
-                    input:checked + .slider:before {
-                      -webkit-transform: translateX(13px);
-                      -ms-transform: translateX(13px);
-                      transform: translateX(13px);
-                    }
-
-                    /* Rounded sliders */
-                    .slider.round {
-                      border-radius: 17px;
-                    }
-
-                    .slider.round:before {
-                      border-radius: 50%;
-                    }
-
-
+                    
                   `}
                 >
-                  <label class="switch">
-                    <input type="checkbox" checked={this.props.darkmode} onChange={(e)=>this.props.onToggle(e)}/>
-                    <span class="slider round"></span>
-                  </label>
                   
                   <a
                     href="https://www.facebook.com/dailybruin"
