1820a3f24c70b68cf05327b650082b94b632dc25
BrysonXiao <40129652+BrysonXiao@users.noreply.github.com>
2023-02-15 14:58:26 -0800

    Changed some hardcoded color values to reference global colors

--- components/EmailSignUp/index.jsx
+++ components/EmailSignUp/index.jsx
@@ -99,7 +99,7 @@ export default class EmailPopUp extends React.Component {
                 font-family: Playfair Display;
                 font-style: normal;
                 font-weight: bold;
-                color: #0080c6;
+                color: ${globals.DBblue};
                 font-size: 40px;
                 // text-align: left;
                 text-align: center;
