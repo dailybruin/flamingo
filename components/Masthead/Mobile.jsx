7f549620df353af85960775b5eaa714c259a0e81
Kaylyn Phan <kaylynkhowong@gmail.com>
2023-02-22 21:45:51 -0800

    change donate links

--- components/Masthead/Mobile.jsx
+++ components/Masthead/Mobile.jsx
@@ -384,7 +384,7 @@ export default class Mobile extends React.Component {
               Advertise
             </a>
             <a
-              href="https://giving.ucla.edu/campaign/?&amount=100&OrgType=C&OrgNum=11300&fund=31601O&_ga=2.101555268.497205227.1521136411-359345519.1518723170"
+              href="https://giving.ucla.edu/campaign/Donate.aspx?Fund=63255C&AutoFN=Y"
               target="_blank"
               rel="noopener"
               css={css`
