8515b8de7e91f5b5e1b743bac5fcb08b79a997c0
Caroline DebBaruah <carolinedebbaruah@MacBook-Pro-15.local>
2024-12-04 21:15:50 -0800

    install webpack v5

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 const withImages = require("next-images");
 
 module.exports = withImages();
+
+module.exports = {
+  images: {
+    disableStaticImages: true
+  }
+}
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
