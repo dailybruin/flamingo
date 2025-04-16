3806dc08aef029a78d3a84f81c0c94dc670e98d4
Ed Nawrocki <eddnawrocki@gmail.com>
2025-04-16 15:48:52 -0700

    SWR for about.jsx + redis

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,19 +1,18 @@
+// pages/[slug].tsx
 import PageWrapper from "../layouts/PageWrapper";
-import React, { Component } from "react";
-import { Config } from "../config.js";
-
+import React from "react";
+import useSWR from 'swr';
 import PageLayout from "../layouts/Page";
 
-class Page extends Component {
-  static async getInitialProps(context) {
-    const { slug } = context.query;
-    const pageRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/pages/227543`);
-    const page = await pageRes.json();
-    return { page };
-  }
-  render() {
-    return <PageLayout page={this.props.page} />;
-  }
+const fetcher = (url) => fetch(url).then(res => res.json());
+
+function Page({ slug }) {
+  const { data, error, isLoading } = useSWR(`/api/227543`, fetcher);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading page</div>;
+
+  return <PageLayout page={data} />;
 }
 
 export default PageWrapper(Page);
