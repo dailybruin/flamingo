317faa3302a2b11635ca6dfba02ea71706306514
Narek Germirlian <ngermirlian@gmail.com>
2025-04-21 19:37:25 -0700

    Added TTL functionality on existing redis implementations

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,7 +7,8 @@ import PageLayout from "../layouts/Page";
 const fetcher = (url) => fetch(url).then(res => res.json());
 
 function Page({ slug }) {
-  const { data, error, isLoading } = useSWR(`/api/227543`, fetcher);
+  const TTL = 432000  // Update once every 5 days
+  const { data, error, isLoading } = useSWR(`/api/227543?ttl=${TTL}`, fetcher);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error loading page</div>;
--- pages/staff.jsx
+++ pages/staff.jsx
@@ -8,7 +8,8 @@ import PageLayout from "../layouts/Page";
 const fetcher = (url) => fetch(url).then(res => res.json());
 
 function Page() {
-  const { data, error, isLoading } = useSWR(`/api/10`, fetcher);
+  const TTL = 432000  // Update once every 5 days
+  const { data, error, isLoading } = useSWR(`/api/10?ttl=${TTL}`, fetcher);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error loading page</div>;
