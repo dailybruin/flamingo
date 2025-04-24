53134dcd127be3b0253fb18544645202767d5aeb
Narek Germirlian <ngermirlian@gmail.com>
2025-04-23 21:12:26 -0700

    Changed Full article card to use NextJS <Image>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withImages = require("next-images");
 module.exports = withImages({
   swcMinify: true, // Enable SWC-based minification
   images: {
+    domains: ['wp.dailybruin.com'],
     disableStaticImages: true,
   },
 });
