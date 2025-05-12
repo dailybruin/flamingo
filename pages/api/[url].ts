96c01725e80ed110b1c2fd3a87a08724820c1d6a
Narek Germirlian <ngermirlian@gmail.com>
2025-05-11 20:11:06 -0700

    Added error checking to redis checks, changed TTL rules

--- pages/api/[url].ts
+++ pages/api/[url].ts
@@ -2,25 +2,53 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import Redis from 'ioredis';
 
-const redis = new Redis("localhost:6379")
+const redis = new Redis({
+    host: 'localhost',
+    port: 6379,
+    connectTimeout: 500,       // fail faster
+    retryStrategy: (times) => {
+      if (times >= 3) return null; // stop retrying after 3 attempts
+      return Math.min(times * 100, 1000);
+    },
+  });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { url, ttl } = req.query;
-    const TTL = parseInt(ttl as string);
+    let TTL = parseInt(ttl as string);
     const URL = url as string;
-    
-    try {
-        const cached = await redis.get(URL);
-        if (cached) {
-            return res.status(200).json(JSON.parse(cached));
-        }
 
-        // const pageRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/pages/${id}`);
+    if (isNaN(TTL)) {
+        TTL = 0;
+    }
+    
+    let cached: string | null = null;
+
+    // Try to get cache
+    try {
+        cached = await redis.get(URL);
+    } catch (err) {
+        console.warn('Redis get failed, continuing without cache.');
+    }
+
+    // Cache hit
+    if (cached) {
+        return res.status(200).json(JSON.parse(cached));
+    }
+
+    // If no cache, then fetch from WP and try to set cache
+    try {
+        // Fetch from WP
         const pageRes = await fetch(URL);
         if (!pageRes.ok) throw new Error('Failed to fetch from WP');
 
         const page = await pageRes.json();
-        await redis.set(URL, JSON.stringify(page), "EX", TTL);
+
+        // Try to set cache
+        try {
+            await redis.set(URL, JSON.stringify(page), "EX", TTL);
+        } catch (err) {
+            console.warn('Redis set failed, continuing without cache.');
+        }
 
         return res.status(200).json(page);
     } catch (err) {
