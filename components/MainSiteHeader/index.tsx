e5fa08cefb35455fb2b04886073a48e5ce8f9870
Ed Nawrocki <eddnawrocki@gmail.com>
2025-05-29 13:24:27 -0700

    Commented out busted header tsx files

--- components/MainSiteHeader/index.tsx
+++ components/MainSiteHeader/index.tsx
@@ -1,57 +1,57 @@
-import * as React from 'react'
-/** @jsxImportSource @emotion/react */
-import { css, jsx } from '@emotion/core'
+// import * as React from 'react'
+// /** @jsxImportSource @emotion/react */
+// import { css, jsx } from '@emotion/core'
 
-import * as MainSiteStyles from '../globals'
-import HeaderBar from './HeaderBar'
+// import * as MainSiteStyles from '../globals'
+// import HeaderBar from './HeaderBar'
 
-interface MainSiteHeaderLink {
-  /** Category name */
-  category: string
-  /** Link location */
-  categoryURL: string
-}
+// interface MainSiteHeaderLink {
+//   /** Category name */
+//   category: string
+//   /** Link location */
+//   categoryURL: string
+// }
 
-interface MainSiteHeaderProps {
-  /** Array of links that refer to sections on the main site */
-  links: MainSiteHeaderLink[]
-  /** Display smaller version with hamburger */
-  hamburger: boolean
-}
+// interface MainSiteHeaderProps {
+//   /** Array of links that refer to sections on the main site */
+//   links: MainSiteHeaderLink[]
+//   /** Display smaller version with hamburger */
+//   hamburger: boolean
+// }
 
-class MainSiteHeader extends React.Component<MainSiteHeaderProps> {
-  constructor(props: MainSiteHeaderProps) {
-    super(props)
-  }
+// class MainSiteHeader extends React.Component<MainSiteHeaderProps> {
+//   constructor(props: MainSiteHeaderProps) {
+//     super(props)
+//   }
 
-  public render() {
-    return (
-      <div>
-        <div
-          css={css`
-            ${MainSiteStyles.mediaMobileBreakpoint} {
-              display: block;
-            }
-            display: none;
-            background-color: #fff;
-          `}
-        >
-          <HeaderBar {...this.props} hamburger={true} />
-        </div>
-        <div
-          css={css`
-            ${MainSiteStyles.mediaMobileBreakpoint} {
-              display: none;
-            }
-            display: block;
-            background-color: #fff;
-          `}
-        >
-          <HeaderBar {...this.props} hamburger={this.props.hamburger} />
-        </div>
-      </div>
-    )
-  }
-}
+//   public render() {
+//     return (
+//       <div>
+//         <div
+//           css={css`
+//             ${MainSiteStyles.mediaMobileBreakpoint} {
+//               display: block;
+//             }
+//             display: none;
+//             background-color: #fff;
+//           `}
+//         >
+//           <HeaderBar {...this.props} hamburger={true} />
+//         </div>
+//         <div
+//           css={css`
+//             ${MainSiteStyles.mediaMobileBreakpoint} {
+//               display: none;
+//             }
+//             display: block;
+//             background-color: #fff;
+//           `}
+//         >
+//           <HeaderBar {...this.props} hamburger={this.props.hamburger} />
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
-export default MainSiteHeader
+// export default MainSiteHeader
