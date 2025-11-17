d6194e17b91fef0cb4f7eb6ad6c2d1be3dd30028
Willjianger9 <1jiangwil@gmail.com>
2025-11-17 15:22:13 -0800

    uncategorized conversion

--- pages/category/uncategorized.jsx
+++ pages/category/uncategorized.jsx
@@ -1,12 +1,10 @@
 // Should take precedence over [slug].jsx
 import PageWrapper from "../../layouts/PageWrapper";
-import React, { Component } from "react";
+import React from "react";
 import Error from "next/error";
 
-class Uncategorized extends Component {
-  render() {
-    return <Error statusCode={404} />;
-  }
+function Uncategorized() {
+  return <Error statusCode={404} />;
 }
 
 export default PageWrapper(Uncategorized);
