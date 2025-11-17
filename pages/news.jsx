41fd8a9fbec1f29793734c226403ef1dd72b34e0
Willjianger9 <1jiangwil@gmail.com>
2025-11-17 13:25:11 -0800

    new conversion from class to functional

--- pages/news.jsx
+++ pages/news.jsx
@@ -1,19 +1,17 @@
 import PageWrapper from "../layouts/PageWrapper";
-import React, { Component } from "react";
+import React from "react";
 
-class Page extends Component {
-  render() {
-    // If the window isn't loaded yet, don't let it throw an error
-    if (typeof window === "undefined") {
-      <p>
-        Oops! We meant to redirect you to https://dailybruin.com/category/news
-      </p>;
-    } else {
-      // Redirect
-      window.location.href = "https://dailybruin.com/category/news";
-    }
-    return null;
+function Page() {
+  // If the window isn't loaded yet, don't let it throw an error
+  if (typeof window === "undefined") {
+    <p>
+      Oops! We meant to redirect you to https://dailybruin.com/category/news
+    </p>;
+  } else {
+    // Redirect
+    window.location.href = "https://dailybruin.com/category/news";
   }
+  return null;
 }
 
 export default PageWrapper(Page);
