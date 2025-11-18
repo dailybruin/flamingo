e79732be79c60e9c2a86c5795473435bf5a2f415
Willjianger9 <1jiangwil@gmail.com>
2025-11-17 16:45:32 -0800

    Rest of conversions

--- pages/author/[slug].jsx
+++ pages/author/[slug].jsx
@@ -1,53 +1,52 @@
 import PageWrapper from "../../layouts/PageWrapper";
-import React, { Component } from "react";
+import React from "react";
 import Error from "next/error";
 import { Config } from "../../config.js";
 import Head from "next/head";
 
 import AuthorLayout from "../../layouts/Author";
 
-class Author extends Component {
-  static async getInitialProps(context) {
-    const { slug } = context.query;
-    const authorRes = await fetch(
-      `${Config.apiUrl}/wp-json/wp/v2/users?slug=${slug}`
-    );
-    const author = await authorRes.json();
-    const postsRes = await fetch(
-      `${Config.apiUrl}/wp-json/wp/v2/posts?_embed&filter[author_name]=${slug}&categories_exclude=27179,27127` // 27179 is the category id of breaking feed posts
-    );
-    const posts = await postsRes.json();
-    const classifiedsRes = await fetch(
-      `${Config.apiUrl}/wp-json/wp/v2/classifieds?_embed&Featured=3`
-    );
-    const classifieds = await classifiedsRes.json();
-    return { author, posts, classifieds };
+function Author({ author, posts, classifieds }) {
+  if (author == undefined || author.length == 0) {
+    return <Error statusCode={404} />;
   }
-  render() {
-    if (this.props.author == undefined || this.props.author.length == 0) {
-      return <Error statusCode={404} />;
-    }
-    return (
-      <>
-        <Head>
-          <title>{this.props.author[0].name + " - Daily Bruin"}</title>
-        </Head>
-        <AuthorLayout
-          author={this.props.author[0]}
-          posts={this.props.posts}
-          classifieds={this.props.classifieds.map(c => {
-            return {
-              category: {
-                name: c._embedded["wp:term"][1][0].name,
-                url: c._embedded["wp:term"][1][0].link
-              },
-              content: { name: c.content.rendered, url: c.link }
-            };
-          })}
-        />
-      </>
-    );
-  }
+  return (
+    <>
+      <Head>
+        <title>{author[0].name + " - Daily Bruin"}</title>
+      </Head>
+      <AuthorLayout
+        author={author[0]}
+        posts={posts}
+        classifieds={classifieds.map(c => {
+          return {
+            category: {
+              name: c._embedded["wp:term"][1][0].name,
+              url: c._embedded["wp:term"][1][0].link
+            },
+            content: { name: c.content.rendered, url: c.link }
+          };
+        })}
+      />
+    </>
+  );
 }
+
+Author.getInitialProps = async (context) => {
+  const { slug } = context.query;
+  const authorRes = await fetch(
+    `${Config.apiUrl}/wp-json/wp/v2/users?slug=${slug}`
+  );
+  const author = await authorRes.json();
+  const postsRes = await fetch(
+    `${Config.apiUrl}/wp-json/wp/v2/posts?_embed&filter[author_name]=${slug}&categories_exclude=27179,27127` // 27179 is the category id of breaking feed posts
+  );
+  const posts = await postsRes.json();
+  const classifiedsRes = await fetch(
+    `${Config.apiUrl}/wp-json/wp/v2/classifieds?_embed&Featured=3`
+  );
+  const classifieds = await classifiedsRes.json();
+  return { author, posts, classifieds };
+};
 
 export default PageWrapper(Author);
