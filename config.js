3ae95ab0f262cb2ecd9eda1bf0e56815a8dc37a6
Narek Germirlian <ngermirlian@gmail.com>
2026-01-13 19:38:54 -0800

    Add shared fetch functions, modularize index.jsx landing page, convert getInitialProps to getStaticProps

--- config.js
+++ config.js
@@ -3,3 +3,33 @@ export const Config = {
   apiUrl: "https://wp.dailybruin.com",
   articleCardFields: ""
 };
+
+/* Each of these tag ids correspond to the tag on Wordpress */
+export const TAG_IDS = {
+  aTAGID: 4847,   // Db-story-a
+  bTAGID: 4850,   // Db-story-b
+  c1TAGID: 4849,  // etc
+  c2TAGID: 4851,
+  d1TAGID: 4862,
+  d2TAGID: 4863,
+  gTAGID: 27530,
+  m1TAGID: 4854,
+  f1TAGID: 22896,
+  f2TAGID: 22897,
+  iTAGID: 27532,
+  jTAGID: 27534,
+  kTAGID: 27535,
+  lTAGID: 27536,
+  hTAGID: 27531
+};
+
+/* Each of these categories has a corresponding WordPress ID */
+export const CAT_IDS = {
+  quadCATID: 12848,
+  newsCATID: 1424,
+  enterpriseCATID: 21602,
+  opinionCATID: 1460,
+  artsCATID: 1454,
+  sportsCATID: 1431
+}
+
