eaeffb676f6449d6f47a13e81747425da2fc81d2
Meet Modi <67122847+modi2meet@users.noreply.github.com>
2026-01-21 16:46:23 -0800

    refactor: extract Desktop Masthead into smaller components and shared styles (#199)
    * refactor: extract Desktop Masthead into smaller components and shared styles
    
    * Remove unused state variable
    
    * Replace masthead DOM mutations with state-driven styles
    
    * Bring social media icons a bit higher
    
    ---------
    
    Co-authored-by: Narek Germirlian <ngermirlian@gmail.com>

--- components/Masthead/Desktop.jsx
+++ components/Masthead/Desktop.jsx
@@ -1,29 +1,37 @@
 import * as React from "react";
 /** @jsxImportSource @emotion/react */
-import { Global, css, jsx } from "@emotion/core";
-import dayjs from "dayjs";
-import Head from "next/head";
-
-import * as globals from "../globals";
+import { css } from "@emotion/core";
 
 import logo from "./dailybruin.svg";
-import menuIcon from "./menu.svg";
-import searchIcon from "./search.svg";
-import minisearchIcon from "./minisearch.svg";
-
-let expandedHeight = "106px";
-let collapsedHeight = "60px";
+import {
+  DateDisplay,
+  TopNavLinks,
+  SearchBar,
+  CategoryLinks
+} from "./DesktopComponents";
+import {
+  expandedHeight,
+  collapsedHeight,
+  LOGO_EXPANDED_HEIGHT,
+  LOGO_COLLAPSED_HEIGHT,
+  mastheadContainerStyles,
+  headerRowStyles,
+  logoContainerStyles,
+  logoStyles,
+  rightColumnStyles,
+  actionsContainerStyles,
+  dividerStyles,
+  dividerLineStyles,
+  categoriesContainerStyles
+} from "./DesktopStyles";
 
 export default class Desktop extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      menuExpanded: true,
-      searchExpanded: false
+      menuExpanded: true
     };
-    this.MastheadCard = React.createRef();
     this.SearchBar = React.createRef();
-    this.Logo = React.createRef();
 
     this.isScrolled = this.isScrolled.bind(this);
     this.expandMenu = this.expandMenu.bind(this);
@@ -52,23 +60,14 @@ export default class Desktop extends React.Component {
   }
 
   expandMenu() {
-    const block = this.MastheadCard.current;
-    const logo = this.Logo.current;
     if (!this.state.menuExpanded) {
-      block.style.height = expandedHeight;
-      logo.style.height = "60px";
-      this.state.menuExpanded = true;
+      this.setState({ menuExpanded: true });
     }
   }
 
   collapseMenu() {
-    const block = this.MastheadCard.current;
-    const logo = this.Logo.current;
     if (this.state.menuExpanded) {
-      block.style.height = collapsedHeight;
-      logo.style.height = "48px";
-
-      this.state.menuExpanded = false;
+      this.setState({ menuExpanded: false });
     }
   }
 
@@ -77,111 +76,26 @@ export default class Desktop extends React.Component {
   }
 
   render() {
-    let date = dayjs();
-    let today = date.format("dddd, MMM D, YYYY");
-    let renderedCategories = [];
-    if (this.props.categories != null) {
-      for (let i in this.props.categories) {
-        renderedCategories.push(
-          <a
-            key={i}
-            href={this.props.categories[i].as}
-            css={css`
-              display: table-cell;
-              text-align: center;
-              padding: 8px 4px;
-              font-family: ${globals.menuFont};
-              font-size: 13px;
-              font-weight: bold;
-              text-decoration: none;
-              text-transform: uppercase;
-              color: #000;
-              white-space: nowrap;
-              &:hover {
-                text-decoration: underline;
-              }
-
-              &.isSticky {
-                background-color: red;
-              }
-            `}
-          >
-            {this.props.categories[i].name}
-          </a>
-        );
-      }
-    }
     return (
-      <React.Fragment>
       <div
-        id="masthead" 
+        id="masthead"
         ref={this.MastheadCard}
-        css={css`
-          background: #ffffff;
-          box-shadow: ${globals.cardShadow};
-          overflow: hidden;
-          height: ${expandedHeight};
-          transition: height 250ms cubic-bezier(0.25, 0.8, 0.25, 1);
-          position: sticky;
-          top: 0;
-          z-index: 10;
-          margin: 6px;
-        `}
+        css={mastheadContainerStyles(
+          this.state.menuExpanded ? expandedHeight : collapsedHeight
+        )}
       >
-        <div
-          css={css`
-            padding: 6px 18px;
-            display: table;
-            table-layout: fixed;
-            width: 100%;
-            vertical-align: middle;
-          `}
-        >
-          <div
-            css={css`
-              display: table-cell;
-              text-align: left;
-              white-space: nowrap;
-              vertical-align: middle;
-              @media (max-width: 900px) {
-                display: none;
-              }
-            `}
-          >
-            <h2
-              css={css`
-                white-space: nowrap;
-                display: inline-block;
-                margin: 0;
-                font-family: ${globals.headlineFont};
-                font-style: normal;
-                font-weight: 550;
-                font-size: 16px;
-              `}
-            >
-              {today}
-            </h2>
-          </div>
-          <div
-            css={css`
-              display: table-cell;
-              text-align: center;
-              white-space: nowrap;
-            `}
-          >
+        <div css={headerRowStyles}>
+          <DateDisplay />
+
+          <div css={logoContainerStyles}>
             <a
               ref={this.Logo}
               href="/"
-              css={css`
-                display: inline-block;
-                vertical-align: middle;
-                height: 60px;
-                padding: 8px 0;
-                transition: height 250ms cubic-bezier(0.25, 0.8, 0.25, 1);
-                @media (max-width: 600px) {
-                  height: 24px;
-                }
-              `}
+              css={logoStyles(
+                this.state.menuExpanded
+                  ? LOGO_EXPANDED_HEIGHT
+                  : LOGO_COLLAPSED_HEIGHT
+              )}
             >
               <img
                 src={logo}
@@ -189,270 +103,30 @@ export default class Desktop extends React.Component {
                   display: inline-block;
                   height: 100%;
                 `}
-              ></img>
+                alt="Daily Bruin"
+              />
             </a>
           </div>
-          <div
-            css={css`
-              display: table-cell;
-              text-align: right;
-              vertical-align: middle;
-              white-space: nowrap;
-            `}
-          >
-            <div
-              css={css`
-                display: inline-block;
-                position: relative;
-                transition: all 500ms;
-                height: 36px;
-              `}
-            >
-              <div
-                css={css`
-                  display: inline-block;
-                  vertical-align: middle;
-                  margin-top: 2px;
-                  margin-right: 10px;
 
-                  & > a {
-                    padding: 0 4px;
-                    font-family: ${globals.menuFont};
-                    font-weight: bold;
-                    text-transform: uppercase;
-                    line-height: 34px;
-                    font-size: 14px;
-                    color: #000;
-                    display: inline-block;
-                    vertical-align: middle;
-                    height: 36px;
-                  }
-
-                  & > a:hover {
-                    text-decoration: underline;
-                  }
-
-                  @media (max-width: 650px) {
-                    display: none;
-                  }
-                `}
-              >
-                <div
-                  id="social-media"
-                  css={css`
-                    display: inline-block;
-
-                    & a {
-                      margin: 0 6px;
-                      display: inline-block;
-                      vertical-align: middle;
-                    }
-                    & a img {
-                      display: block;
-                      height: 14px;
-                    }
-
-                    & a:hover img {
-                      fill: ${globals.DBblue};
-                    }
-                  `}
-                >
-                  <a
-                    href="https://www.facebook.com/dailybruin"
-                    target="_blank"
-                    rel="noopener"
-                  >
-                    <img src={require("./facebook.svg")} />
-                  </a>
-                  <a
-                    href="https://www.twitter.com/dailybruin"
-                    target="_blank"
-                    rel="noopener"
-                  >
-                    <img src={require("./twitter.svg")} />
-                  </a>
-                  <a
-                    href="https://www.instagram.com/dailybruin"
-                    target="_blank"
-                    rel="noopener"
-                  >
-                    <img src={require("./instagram.svg")} />
-                  </a>
-                  <a
-                    href="http://eepurl.com/cFEiZX"
-                    target="_blank"
-                    rel="noopener"
-                  >
-                    <img src={require("./mail.svg")} />
-                  </a>
-                  <a
-                    href="https://www.youtube.com/user/ucladailybruin"
-                    target="_blank"
-                    rel="noopener"
-                  >
-                    <img src={require("./youtube.png")} />
-                  </a>
-                </div>
-                <a href="/advertise">Advertise</a>
-                <a href="https://uclastudentmedia.com/donate/">
-                  Donate
-                </a>
-                <a href="/submit">Submit</a>
-              </div>
-              <div
-                css={css`
-                  display: inline-block;
-                  position: relative;
-                  transition: all 500ms;
-                  height: 36px;
-                  width: 36px;
-                  vertical-align: middle;
-                `}
-              >
-                <form method="get" action="/search">
-                  <input
-                    ref={this.SearchBar}
-                    id="SearchBar"
-                    type="text"
-                    name="q"
-                    placeholder="search"
-                    pattern="\S+.*"
-                    css={css`
-                      position: absolute;
-                      right: 0;
-                      z-index: 10;
-                      height: 36px;
-                      background-color: #000;
-                      color: #000;
-                      resize: none;
-                      transition: width 500ms cubic-bezier(0.25, 0.8, 0.25, 1),
-                        color 300ms cubic-bezier(0.25, 0.8, 0.25, 1);
-                      width: 0;
-                      padding: 0;
-                      border: none;
-                      outline: none;
-                      line-height: 36px;
-                      font-size: 18px;
-                      font-family: ${globals.menuFont};
-                      font-weight: bold;
-                      &:focus {
-                        width: 250px;
-                        padding: 0 36px 0 6px;
-                        color: #fff;
-                      }
-                      &:focus + input {
-                        display: block;
-                      }
-                      &:focus + input + #Masthead__SearchIconBox {
-                        background-color: #000;
-                      }
-                    `}
-                  ></input>
-                  <input
-                    type="submit"
-                    value=""
-                    css={css`
-                      position: absolute;
-                      z-index: 12;
-                      width: 36px;
-                      height: 36px;
-                      right: 0;
-                      top: 0;
-                      border: none;
-                      padding: 6px;
-                      cursor: pointer;
-                      color: #fff;
-                      outline: none;
-                      display: none;
-                      background-color: #000;
-                      background-image: url(${minisearchIcon});
-                      background-repeat: no-repeat;
-                      background-size: 24px;
-                      background-position: 6px;
-                      &:hover {
-                        display: block;
-                      }
-                      &:hover ~ input {
-                        width: 250px;
-                        padding: 0 36px 0 6px;
-                        color: #fff;
-                      }
-                      &:focus {
-                        outline: none;
-                        display: block;
-                      }
-                    `}
-                  />
-                  <div
-                    css={css`
-                      position: absolute;
-                      z-index: 11;
-                      right: 0;
-                      top: 0;
-                      border: none;
-                      padding: 0;
-                      cursor: pointer;
-                      background-color: transparent;
-                      outline: none;
-                      &:focus {
-                        outline: none;
-                      }
-                    `}
-                    onClick={this.expandSearch}
-                  >
-                    <img
-                      id="Masthead__SearchIconBox"
-                      css={css`
-                        display: inline-block;
-                        vertical-align: middle;
-                        transition: all 200ms;
-                        transition-delay: 100ms;
-                        width: 36px;
-                        height: 36px;
-                      `}
-                      src={searchIcon}
-                    ></img>
-                  </div>
-                </form>
-              </div>
+          <div css={rightColumnStyles}>
+            <div css={actionsContainerStyles}>
+              <TopNavLinks />
+              <SearchBar
+                searchBarRef={this.SearchBar}
+                onExpandSearch={this.expandSearch}
+              />
             </div>
           </div>
         </div>
-        <div
-          css={css`
-            padding: 0 12px;
-          `}
-        >
-          <div
-            css={css`
-              width: 100%;
-              height: 1px;
-              background-color: #000;
-            `}
-          ></div>
+
+        <div css={dividerStyles}>
+          <div css={dividerLineStyles}></div>
         </div>
-        <div
-          css={css`
-            background-color: #fff;
-            overflow-x: scroll;
-            &::-webkit-scrollbar {
-              display: none;
-            }
-          `}
-        >
-          <div
-            css={css`
-              padding: 0 12px;
-              display: table;
-              width: 100%;
-              box-sizing: border-box;
-            `}
-          >
-            {renderedCategories}
-          </div>
+
+        <div css={categoriesContainerStyles}>
+          <CategoryLinks categories={this.props.categories} />
         </div>
       </div>
-      </React.Fragment>
     );
   }
 }
---
+++ components/Masthead/DesktopStyles.js
@@ -0,0 +1,117 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/core";
+import * as globals from "../globals";
+
+// Constants
+export const expandedHeight = "106px";
+export const collapsedHeight = "60px";
+export const ACTION_HEIGHT = "36px";
+export const ACTION_WIDTH = "36px";
+export const SEARCH_EXPANDED_WIDTH = "250px";
+export const LOGO_EXPANDED_HEIGHT = "60px";
+export const LOGO_COLLAPSED_HEIGHT = "48px";
+export const Z_INDEX_SEARCH_INPUT = 10;
+export const Z_INDEX_SEARCH_ICON = 11;
+export const Z_INDEX_SEARCH_SUBMIT = 12;
+
+// Common utility styles
+export const inlineBlockVerticalMiddle = css`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+export const whiteSpaceNowrap = css`
+  white-space: nowrap;
+`;
+
+export const outlineNone = css`
+  outline: none;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const menuLinkStyles = css`
+  font-family: ${globals.menuFont};
+  font-weight: bold;
+  text-transform: uppercase;
+  color: ${globals.black};
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const absoluteTopRight = (zIndex = 0) => css`
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: ${zIndex};
+`;
+
+export const mastheadContainerStyles = (height) => css`
+  background: ${globals.white};
+  box-shadow: ${globals.cardShadow};
+  overflow: hidden;
+  height: ${height};
+  transition: height 250ms cubic-bezier(0.25, 0.8, 0.25, 1);
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  margin: 6px;
+`;
+
+export const headerRowStyles = css`
+  padding: 6px 18px;
+  display: table;
+  table-layout: fixed;
+  width: 100%;
+  vertical-align: middle;
+`;
+
+export const logoContainerStyles = css`
+  display: table-cell;
+  text-align: center;
+  ${whiteSpaceNowrap};
+`;
+
+export const logoStyles = (height) => css`
+  ${inlineBlockVerticalMiddle};
+  height: ${height};
+  padding: 8px 0;
+  transition: height 250ms cubic-bezier(0.25, 0.8, 0.25, 1);
+  @media (max-width: 600px) {
+    height: 24px;
+  }
+`;
+
+export const rightColumnStyles = css`
+  display: table-cell;
+  text-align: right;
+  vertical-align: middle;
+  ${whiteSpaceNowrap};
+`;
+
+export const actionsContainerStyles = css`
+  ${inlineBlockVerticalMiddle};
+  position: relative;
+  transition: all 500ms;
+  height: ${ACTION_HEIGHT};
+`;
+
+export const dividerStyles = css`
+  padding: 0 12px;
+`;
+
+export const dividerLineStyles = css`
+  width: 100%;
+  height: 1px;
+  background-color: ${globals.black};
+`;
+
+export const categoriesContainerStyles = css`
+  background-color: ${globals.white};
+  overflow-x: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
